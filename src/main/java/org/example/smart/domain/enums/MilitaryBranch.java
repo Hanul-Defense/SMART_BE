95e3f2eaf647e736464d8b8a3a9b056e3a9a4b04
ImHyungsuk <limhs8630@gmail.com>
2025-05-14 18:59:40 +0900

    [Feat] 군종 & 계급 문자열 값 Enum으로 변환

--- src/main/java/org/example/smart/domain/enums/MilitaryBranch.java
+++ src/main/java/org/example/smart/domain/enums/MilitaryBranch.java
@@ -9,4 +9,14 @@ public enum MilitaryBranch {
     ARMY("육군"), AIR("공군"), NAVY("해군"), MARINE("해병대"),
     ;
     private final String name;
+
+    public static MilitaryBranch getMilitaryBranchByName(String name) {
+        return switch (name) {
+            case ("육군") -> ARMY;
+            case ("공군") -> AIR;
+            case ("해군") -> NAVY;
+            case ("해병대") -> MARINE;
+            default -> null;
+        };
+    }
 }
