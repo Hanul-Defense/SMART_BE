ca1d2f3814fe4887a34043297c67f0d4c30d11bd
ImHyungsuk <limhs8630@gmail.com>
2025-05-23 21:24:42 +0900

    [Feat] CustomUserDetails 구현

---
+++ src/main/java/org/example/smart/security/model/CustomUserDetails.java
@@ -0,0 +1,36 @@
+package org.example.smart.security.model;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+	private final Long userId;
+	private final String serviceNumber;
+	private final String password;
+	private final String name;
+
+	private final Collection<? extends GrantedAuthority> authorities;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+		return serviceNumber;
+	}
+}
