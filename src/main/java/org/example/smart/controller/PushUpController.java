b77e00bd945d0ebb82178767362e8c44b3f1608b
ImHyungsuk <limhs8630@gmail.com>
2025-05-31 21:03:02 +0900

    [Feat] 푸시업 기록 가져오기 api 연결

--- src/main/java/org/example/smart/controller/PushUpController.java
+++ src/main/java/org/example/smart/controller/PushUpController.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -50,5 +51,14 @@ public class PushUpController {
 			.body(new BaseResponseDto<>(HttpStatus.CREATED.value(),
 				estimationService.postEstimation(AuthenticationUtil.getSoldierId(authentication), postEstimationDto),
 				null));
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<BaseResponseDto<ResponseEstimationRecordDto>> getPushUpRecord(
+		@PathVariable("id") Long pushUpId
+	) {
+		return ResponseEntity.status(HttpStatus.OK)
+			.body(new BaseResponseDto<>(HttpStatus.OK.value(), "",
+				estimationService.getEstimationRecord(pushUpId)));
 	}
 }
